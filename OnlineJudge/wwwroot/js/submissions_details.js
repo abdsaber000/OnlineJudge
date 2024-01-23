ab7dd9d4f8b73d4f04f5789ac6db59aca6f396e0
abdsaber000 <abdsaber000@git.com>
2024-01-23 11:32:04 +0200

    Style submissions index page

---
+++ OnlineJudge/wwwroot/js/submissions_details.js
@@ -0,0 +1,13 @@
+﻿const VredictEls = document.querySelectorAll(".vredict");
+
+VredictEls.forEach(VredictEl => {
+    const statusStr = VredictEl.innerText;
+    console.log(statusStr);
+    if (statusStr === "Accepted") {
+        VredictEl.style.color = 'green';
+    } else if (statusStr === "In queue") {
+        VredictEl.style.color = 'gray';
+    } else {
+        VredictEl.style.color = 'red';
+    }
+})
